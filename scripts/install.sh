18a95081af652883431c8adf27858dff631ae340
Jean-Baptiste Cordonnier <jbcdnr@gmail.com>
2018-06-29 10:48:27 +0200

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-script_path=/mlodata1/gpu-monitor/scripts
+set -x
+
+MONITOR_PATH=$(readlink -f "$(dirname "$0")/..")
+SCRIPT_PATH="$MONITOR_PATH/scripts"
 
 # check that bc is installed
 if ! bc -v >> /dev/null; then
@@ -9,6 +12,19 @@ fi
 
 # add cron tasks
 (crontab -l 2>/dev/null; \
-  echo "*/5 * * * * $script_path/gpu-check.sh $(hostname) > /dev/null 2>&1"; \
-  echo "0 */2 * * * $script_path/gpu-check.sh kill > /dev/null 2>&1; $script_path/gpu-check.sh $(hostname) > /dev/null 2>&1") \
+  echo "*/5 * * * * $SCRIPT_PATH/gpu-check.sh $(hostname) > /dev/null 2>&1"; \
+  echo "0 */2 * * * $SCRIPT_PATH/gpu-check.sh kill > /dev/null 2>&1; $SCRIPT_PATH/gpu-check.sh $(hostname) > /dev/null 2>&1") \
 | crontab -
+
+# build docker images
+DOCKER_PATH="$MONITOR_PATH/docker"
+echo "Building docker images in $DOCKER_PATH..."
+MONITOR_UID=$(id -u)
+MONITOR_GID=$(getent group MLO-unit | awk -F: '{printf "%d\n", $3}')
+docker build --build-arg uid=$MONITOR_UID --build-arg gid=$MONITOR_GID -t mlo-nginx $DOCKER_PATH/nginx
+docker build --build-arg uid=$MONITOR_UID --build-arg gid=$MONITOR_GID -t mlo-php $DOCKER_PATH/php
+echo "Done."
+
+# start web interface
+docker-compose -f $DOCKER_PATH/docker-compose.yml up -d
+
